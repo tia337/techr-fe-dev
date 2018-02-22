49bc8ec4a97a6da64ac5d98fb8a1a58493a644ad
YaroslavPetryk <yarulo95@inbox.ru>
2018-02-22 11:56:26 +0200

    added fixed height, scroll, and body click closing function

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -13,7 +13,7 @@ import { AlertComponent } from '../shared/alert/alert.component';
 import { Socket } from 'ng-socket-io';
 import { PostJobService } from 'app/post-job-page/post-job.service';
 
-import { ContactUsComponent } from "app/contact-us/contact-us.component";
+import { ContactUsComponent } from 'app/contact-us/contact-us.component';
 import { NotificationsComponent } from './notifications/notifications.component';
 
 
@@ -86,16 +86,10 @@ export class HeaderComponent implements OnInit, OnDestroy {
 			this.getLogo();
 		}
 		this._currentUserSubscription = this._login.profile.subscribe(profile => {
-			// console.log('PROFILE: ', profile1);
-			// console.log('CALL CONFIRM ALERT FROM HEADER OnInit');
 			if (profile) {
 				// profile1.fetch().then(profile => {
 				this.currentUser = profile;
-				console.log('profile', profile);
-				console.log(`profile.get('Client_Pointer')`, profile.toJSON().Client_Pointer);
-				console.log(`profile.has('Client_Pointer')`, profile.has('Client_Pointer'));
 				if (!profile.toJSON().Client_Pointer) {
-					console.log("CONFORMATION ALERT");
 					this._root_vcr.clear();
 					this._root_vcr.createComponent(ConfirmationAlertComponent);
 				}
@@ -106,12 +100,9 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
 	closeAdminMenu(event?) {
 		if (event) {
-			// console.log(event.clientY);
 			if (event.clientY < 55) {
 				this._closeAnim = true;
-				// console.log(this.closeAnim);
 				setTimeout(() => {
-					// console.log(this.closeAnim);
 					if (this._closeAnim === true) {
 						this._menuOpened = false;
 						this._closeAnim = false;
@@ -120,9 +111,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
 			}
 		} else {
 			this._closeAnim = true;
-			// console.log(this.closeAnim);
 			setTimeout(() => {
-				// console.log(this._closeAnim);
 				if (this.closeAnim === true) {
 					this._menuOpened = false;
 					this._closeAnim = false;
@@ -133,12 +122,9 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
     closeUserMenu(event?) {
         if (event) {
-            // console.log(event.clientY);
             if (event.clientY < 55) {
                 this._closeUserAnim= true;
-                console.log('_closeUserAnim',this._closeUserAnim);
                 setTimeout(() => {
-                    // console.log(this.closeAnim);
                     if (this._closeUserAnim === true) {
                         this._userMenuOpened = false;
                         this._closeUserAnim = false;
@@ -147,9 +133,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
             }
         } else {
             this._closeUserAnim = true;
-            // console.log(this.closeAnim);
             setTimeout(() => {
-                console.log('_closeUserAnim',this._closeUserAnim);
                 if (this.closeUserAnim === true) {
                     this._userMenuOpened = false;
                     this._closeUserAnim = false;
@@ -162,14 +146,12 @@ export class HeaderComponent implements OnInit, OnDestroy {
 	getLogo() {
 		this._headerService.getClientLogo().then(logo => {
 			this._clientLogo = logo;
-			// console.log(logo._url);
 		});
 	}
 
 	signIn() {
 		this._login.signIn().then(user => {
 			if (!user.has('Client_Pointer')) {
-				console.log('CALL CONFIRM ALERT FROM HEADER SignIn');
 				this._root_vcr.clear();
 				this._root_vcr.createComponent(ConfirmationAlertComponent);
 			}
@@ -186,8 +168,6 @@ export class HeaderComponent implements OnInit, OnDestroy {
 	openAdminMenu() {
 		this._closeAnim = false;
 		this._menuOpened = true;
-		// const menu = this._vcr.createComponent(this._cfr.resolveComponentFactory(AdministrationMenuComponent)).instance;
-		// menu.containerRef = this._vcr;
 	}
 
     openUserMenu() {
@@ -204,23 +184,18 @@ export class HeaderComponent implements OnInit, OnDestroy {
 	}
 
 	test() {
-		console.log("test conformation");
 		this._root_vcr.createComponent(ConfirmationAlertComponent);
 	}
 
 	accessCheck(event) {
 		event.stopPropagation();
-		// console.log('access Check');
 		this._headerService.getAccessLevel().then(alevel => {
 			this.accessLevel = alevel;
 			return this._headerService.getAdmins();
 		}).then(admins => {
-			// console.log(this.accessLevel);
 			if (this.accessLevel === 1) {
-				// console.log('1 access level');
 				this.router.navigate(['/', 'administration', 'billing']);
 			} else if (this.accessLevel > 1) {
-				// console.log('alert');
 				const alert = this._root_vcr.createComponent(AlertComponent);
 				alert.title = 'Access level Required';
 				alert.icon = 'lock';
@@ -230,17 +205,13 @@ export class HeaderComponent implements OnInit, OnDestroy {
 					`. You need to be site admin in order to see your billing information.</a><br><a style = 'white-space:nowrap'><strong>` +
 					admins + `</strong> can set you up as a site admin if needed.</a>` +
 					`<a style = 'white-space:nowrap'>Contact SwipeIn if you need urgent access to Billing and you can't reach your Billing information.</a>`;
-
-				// alert.content = `
-
-				// `;
 				alert.addButton({
 					title: 'Contact SwipeIn',
 					type: 'secondary',
 					onClick: () => {
 						this._root_vcr.clear();
 						let contactForm = this._root_vcr.createComponent(ContactUsComponent);
-						contactForm.contactType = "K9A4lQwYNs";
+						contactForm.contactType = 'K9A4lQwYNs';
 					}
 				});
 				alert.addButton({
@@ -248,7 +219,6 @@ export class HeaderComponent implements OnInit, OnDestroy {
 					type: 'primary',
 					onClick: () => this._root_vcr.clear()
 				});
-				
 			}
 		});
 	}
@@ -273,4 +243,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
 	changeNotifications (notifications: boolean): void {
 		this._notificationsOpened = notifications;
 	}
+
+
+
 }
--- src/app/header/notifications/notifications.component.ts
+++ src/app/header/notifications/notifications.component.ts
@@ -1,5 +1,5 @@
-import { Component, EventEmitter, OnInit, Output, Input} from '@angular/core';
-
+import { Component, EventEmitter, OnInit, Output, Input, ElementRef, Renderer} from '@angular/core';
+// tslint:disable:indent
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
@@ -8,18 +8,28 @@ import { Component, EventEmitter, OnInit, Output, Input} from '@angular/core';
 export class NotificationsComponent implements OnInit {
 
   @Input()
-  notifications;
+  notifications: boolean;
 
-  @Output() changeNotifications: EventEmitter<boolean> = new EventEmitter();
-
-  constructor() { }
+  @Output() notificationsStatus: EventEmitter<boolean> = new EventEmitter();
+  constructor(
+    private _elemenRef: ElementRef,
+    private _renderer: Renderer
+  ) {
+      this._renderer.listenGlobal('body', 'click', (event) => {
+        if (this.notifications === true) {
+          this.notifications = !this.notifications;
+          this.notificationsStatus.emit(this.notifications);
+        }
+        event.stopPropagation();
+    });
+  }
 
   ngOnInit() {
   }
 
-  closeNotifications(notifications: boolean): void {
+  closeNotifications(notifications: boolean, event): void {
       notifications = !notifications;
-      this.changeNotifications.emit(notifications);
-  }  
-
+      this.notificationsStatus.emit(notifications);
+      event.stopPropagation();
+  }
 }
