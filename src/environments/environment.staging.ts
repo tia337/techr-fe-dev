5075cd0925aea5f233f650419ec6bfb02694fda1
vresch <max.vresch@gmail.com>
2018-03-01 13:59:34 +0200

    added ERP url to environmental variables

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -3,6 +3,7 @@ export const environment = {
   APP_ID: 'dOEXXQD7cPjiKTAitzNLdzt5fJWXscFSFYOJmBIZ',
   SERVER_URL: 'https://swipeinmlabtest.herokuapp.com/parse',
   SOCKET_IO: 'https://swipeinmlabtest.herokuapp.com/',
+  REFERRAL_URL: 'https://swipein-referral.herokuapp.com/erp/',
   STRIPE_KEY: 'pk_test_DOxsBoIZIbzfu3y6EafzMDNU',
   GOOGLE_API_KEY: 'AIzaSyBFfAIR1tt4-AOfcsGxc87y-yZMLdrMNbk'
 };
