db43c5980798f8a9b4a77cc04e236b60d4b2cbda
Ярослав Петрик <yaroslavpetryk95@gmail.com>
2018-03-05 15:03:13 +0000

    Revert "added error handler for linkedin auth which refreshs token and calls back signin function (pull request #21)"
    This reverts pull request #21.
    
    > added error handler for linkedin auth which refreshs token and calls back signin function

--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -30,6 +30,7 @@ export class Login {
 				this._global.IN.API.Raw(
 					'/people/~:(id,first-name,last-name,picture-url,headline,location,industry,num-connections,summary,positions,email-address,picture-urls::(original))?format=json'
 				).result( data => {
+					console.log(data);
 					this._parse.Parse.Cloud.run('signUp', {data: data}).then(user => {
 						if (!user.authenticated()) {
 							return this._parse.Parse.User.logIn(user.get('username'), this.getPassword(user.get('username')));
@@ -41,20 +42,12 @@ export class Login {
 						this._router.navigate(['/dashboard']);
 						resolve(user);
 						this._vcr.clear();
+					}, error => {
+						reject(error);
 					});
-					// , error => {
-					// 	reject(error);
-					// });
-				}).error(
-					this.userRefreshToken()
-				);
+				});
 			});
 		});
-	}
-
-	userRefreshToken() {
-		this._global.IN.User.refresh();
-		this.signIn();
 	}
 
 	getPassword(username: string) {
