1ad2faf3a24b685e8aac5fe0c8629c2ca378818a
YaroslavPetryk <yarulo95@inbox.ru>
2018-04-18 14:08:15 +0300

    1

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TimelineService } from './timeline.service';
+import { Socket } from 'ng-socket-io';
+// tslint:disable:indent
 
 @Component({
   selector: 'app-timeline',
@@ -7,9 +10,91 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimelineComponent implements OnInit {
 
-  constructor() { }
+  public timelineArray = [];
+  private _startFrom = 0;
+  private timelineStorage: Array<any> = [];
+  private timelineTempStorage: Array<any> = [];
+  public loader = false;
+  private _timelineQueryLimits = {
+		from: 0,
+		to: 15
+  };
+  private _notificationsArrayLimits = {
+    from: 0,
+    to: 5
+  };
+
+  constructor(
+    private _socket: Socket,
+    private _timelineService: TimelineService
+  ) { }
 
   ngOnInit() {
+    // this._timelineService.emitSocketEventForRecievingTimeline(this._startFrom);
+    // this._timelineService.recieveTimelineFromSocket().subscribe(timeline => {
+    //   console.log(timeline);
+    //   this.sortTimeline(timeline);
+    // });
+    // this._startFrom += 100;
   }
+
+  // sortTimeline (data) {
+  //   this.timelineArray = this._timelineService.sortTimeline(data);
+  //   console.log(this.timelineArray);
+  // }
+
+  // goToJobDetails (contractId: string, stage: number) {
+  //   this._timelineService.goToJobDetails(contractId, stage);
+  // }
+
+  // uploadMoreNotifications (event) {
+  //   if (event.target.scrollHeight - event.target.scrollTop - event.target.offsetHeight === 0) {
+  //     if (this._timelineQueryLimits.from > this._notificationsArrayLimits.from) {
+  //       this.loader = true;
+  //       const slicedArray = this.timelineStorage
+  //         .slice(this._notificationsArrayLimits.from, this._notificationsArrayLimits.to);
+  //       this.timelineTempStorage = this.timelineTempStorage.concat(slicedArray);
+  //       this.timelineArray = this.sortTimeline(this.timelineTempStorage);
+  //       this._notificationsArrayLimits.from += 5;
+  //       this._notificationsArrayLimits.to += 5;
+  //     } else if (this._timelineQueryLimits.from === this._notificationsArrayLimits.from) {
+  //       this.loadNotifications();
+  //     };
+  //   };
+  // };
+
+  // loadNotifications () {
+  //   this.loader = true;
+  //   const date = new Date();
+  //   date.setMonth(date.getMonth() - 3);
+  //   this._parse.execCloud('getAllNotifications',
+  //     {
+  //       userId: this._parse.getCurrentUser().id,
+  //       clientId: this._parse.getCurrentUser().get('Client_Pointer').id,
+  //       limits: this._timelineQueryLimits,
+  //       date: date
+  //     })
+  //     .then(result => {
+  //       const data = JSON.parse(result);
+  //       if (this.notificationsArray.length === 0) {
+  //         this.timelineStorage = data;
+  //         this.timelineTempStorage = this.timelineStorage
+  //           .slice(this._notificationsArrayLimits.from, this._notificationsArrayLimits.to);
+  //         this.notificationsArray =	this.sortNotifications(this.timelineTempStorage);
+  //       } else {
+  //         this.timelineStorage = this.timelineStorage.concat(data);
+  //         const slicedArray = this.timelineStorage
+  //           .slice(this._notificationsArrayLimits.from, this._notificationsArrayLimits.to);
+  //         this.timelineTempStorage = this.timelineTempStorage.concat(slicedArray);
+  //         this.notificationsArray = this.sortNotifications(this.timelineTempStorage);
+  //       }
+  //       this._timelineQueryLimits.from += 15;
+  //       this._timelineQueryLimits.to += 15;
+  //       this._notificationsArrayLimits.from += 5;
+  //       this._notificationsArrayLimits.to += 5;
+	//   	}).catch(error => {
+  //       console.log(error);
+  //     });
+  // }
 
 }
--- src/app/timeline/timeline.service.ts
+++ src/app/timeline/timeline.service.ts
@@ -1,8 +1,98 @@
 import { Injectable } from '@angular/core';
+import { Socket } from 'ng-socket-io';
+import { Observable } from 'rxjs/Observable';
+import { JobBoxService } from '../jobs-page/job-box/job-box.service';
+import { JobDetailsService } from '../job-details/job-details.service';
+import { Parse } from '../parse.service';
+import { Router, Route, ActivatedRoute } from '@angular/router';
+import { DeveloperListType, JobType, ContractStatus } from '../shared/utils';
+
+    // tslint:disable:indent
 
 @Injectable()
 export class TimelineService {
 
-  constructor() { }
+  constructor (
+    private _jobBoxService: JobBoxService,
+    private _jobDetailsService: JobDetailsService,
+    private _ar: ActivatedRoute,
+    private _socket: Socket,
+    private _router: Router,
+    private _parse: Parse,
+    private _route: Route,
+  ) { }
+
+  emitSocketEventForRecievingTimeline (startFrom) {
+    this._socket.emit('give-me-timeline', {startFrom: startFrom});
+  }
+
+  recieveTimelineFromSocket () {
+    const observable = new Observable (observer => {
+      this._socket.on('timeline-fetched', data => {
+        observer.next(data);
+      });
+    });
+    return observable;
+  }
+
+  createDatesArray (data) {
+    const dates = [];
+    const datesToDisplay = [];
+		data.forEach(notification => {
+			const day: Date = new Date(notification._created_at);
+			if (!dates.includes(day.toLocaleDateString())) {
+        dates.push(day.toLocaleDateString());
+        datesToDisplay.push(day);
+      }
+		});
+		return {
+      dates: dates,
+      datesToDisplay: datesToDisplay
+    };
+  }
+
+  sortTimeline(data) {
+		const timelineSorted = [];
+    const dates = this.createDatesArray(data).dates;
+    const datesToDisplay = this.createDatesArray(data).datesToDisplay;
+		let i = 0;
+		dates.forEach(date => {
+			const dayTimeline = [];
+			const timelineArray = [];
+			dayTimeline.push({
+        date: dates[i],
+        dateToDisplay: datesToDisplay[i]
+      });
+			i++;
+			data.forEach(timeline => {
+				const timelineDate: Date = new Date (timeline._created_at);
+				if (timelineDate.toLocaleDateString() === date) {
+					timelineArray.push(timeline);
+				}
+			});
+			dayTimeline.push({ timeline: timelineArray });
+			timelineSorted.push(dayTimeline);
+    });
+    return timelineSorted;
+  }
+  
+  goToJobDetails(contractId: string, stage: number) {
+    const contractQuery = this._parse.Query('Contract');
+    contractQuery.equalTo('objectId', contractId);
+    contractQuery.find().then(contract => {
+      this._router.navigate(['/jobs', contractId]);
+      this._jobBoxService.activeContract = contract;
+      if (contract.get('status') == ContractStatus.draft) {
+        this._router.navigate(['/jobs', contractId]);
+        setTimeout(() => {
+          this._router.navigate(['/jobs', contractId, 'job-overview'], { skipLocationChange: true, relativeTo: this._ar });
+        }, 1);
+      } else {
+      }
+      this._jobDetailsService.activeStage = stage;
+      localStorage.setItem('activeStage', stage.toString());
+    });
+	}
+
 
 }
