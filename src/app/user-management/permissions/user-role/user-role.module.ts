e6bea290df38f7d66f092ade95dffaee81a7b786
bdawie <ibrahimbdawie@gmail.com>
2018-04-30 18:36:08 +0300

    User role Modal

---
+++ src/app/user-management/permissions/user-role/user-role.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserRoleComponent } from './user-role.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [UserRoleComponent]
+})
+export class UserRoleModule { }
