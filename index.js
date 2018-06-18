7e762cf08d0ae9868b38f7bc59fad8e5abea95f7
YaroslavPetryk <yaroslavpetryk95@gmail.com>
2018-06-18 17:44:23 +0300

    release 1.0

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const cors = require('cors');
+const compression = require('compression');
+
+const app = express();
+
+const forceSSL = function () {
+	return function (req, res, next) {
+		if (req.headers['x-forwarded-proto'] !== 'https') {
+			return res.redirect(
+				['https://', req.get('Host'), req.url].join('')
+			);
+		}
+		next();
+	}
+}
+
+app.use(forceSSL());
+app.use(cors());
+app.use(compression());
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', function(req, res) {
+	res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+const port = process.env.PORT || '4200';
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, function() {
+	console.log('Running app');
+});
