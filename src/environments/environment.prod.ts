0c0054968ba365cb69325ac11ecb9ecc071af9ad
YaroslavPetryk <yarulo95@inbox.ru>
2019-03-19 20:45:51 +0200

    added -env=prod to postinstall

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   APP_ID: 'yAvY902wI7a5bH6nwTwdM8I8bswhS2uXr202zREt',
-  SERVER_URL: 'https://swipein.herokuapp.com/parse',
+  SERVER_URL: 'https://tech-hr-be.herokuapp.com',
   SOCKET_IO: 'https://swipein.herokuapp.com/',
   REFERRAL_URL: 'https://referrals.swipein.hr',
   GOOGLE_API_KEY: 'AIzaSyBFfAIR1tt4-AOfcsGxc87y-yZMLdrMNbk',
